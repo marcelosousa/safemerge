f2ce8d18743b82f021a4b12f7bb0836b4c521f7a
marcelosousa <dipython@gmail.com>
2015-02-02 19:03:49 +0000

    updated version of script

--- conflicts.sh
+++ conflicts.sh
@@ -20,18 +20,20 @@ for line in `git log --merges --pretty=format:%P`; do
   # Find base
   base=`git merge-base --octopus $line`
   
-  echo "Merging base=$base with branches=$line..."
+  #echo "Merging base=$base with branches=$line..."
   # Dry run
   result=`git merge-tree $base $line`
   # If the result contains the string changed in both
   # its very likely that there is a conflict 
-  if [[ $result == *"changed in both"* ]]
+  if [[ $result == *"+<<<<<<<"* ]]
   then 
-    echo "Potential conflict!";
     NC=$((NC+1))
+    echo "Conflict $NC!"
+    echo "$NC: git merge-tree $base $line" >> conflict_log.txt
+    git merge-tree $base $line > "conflict_$NC.txt"
   fi 
   NM=$((NM+1))
 done
 
-echo "Number of merges=$NM, number of conflicts=$NC"
+echo "Number of merges=$NM, number of conflicts=$NC" >> conflict_log.txt
 echo "Done"
