c06f1e32deeff704dedca4a5dd4df2d867f1d3ef
marcelosousa <dipython@gmail.com>
2016-01-22 09:34:32 -0500

    moving pp to Printer.hs

--- src/Encoding.hs
+++ src/Encoding.hs
@@ -33,32 +33,6 @@ encode base a b m = main_encoding $ generate_product base a b m
 generate_product :: Program -> Edit -> Edit -> Edit -> ProdProgram
 generate_product base a b m = flatten_product base $ gen_product base a b m
 
-pp_prod_prog :: ProdProgram -> String
-pp_prod_prog (n_e, m, n_x) = 
-  let n_e_s = "Entry label: " ++ n_e
-      n_x_s = "Exit label: " ++ show n_x
-      prog_s = M.foldWithKey pp_prod_prog_line "" m
-  in n_e_s ++ "\n" ++ prog_s ++ n_x_s
-  where
-pp_prod_prog_line pre r@[(ba,pos_ba), (a,pos_a), (b,pos_b), (m,pos_m)] rest =
-  let ba_s = "\t" ++ pre ++ ": " ++ show ba ++ ": " ++ show pos_ba
-      a_s = "; " ++ show a ++ ": " ++ show pos_a
-      b_s = "; " ++ show b ++ ": " ++ show pos_b
-      m_s = "; " ++ show m ++ ": " ++ show pos_m
-  in ba_s ++ a_s ++ b_s ++ m_s ++ "\n" ++ rest
-
-pp_gen_product :: (Label, EditMap, [Label]) -> String
-pp_gen_product (n_e, p, n_x) =
-  let n_e_s = "Entry label: " ++ n_e
-      n_x_s = "Exit label: " ++ show n_x
-      prog_s = M.foldWithKey pp_edit_map "" p
-  in unlines [n_e_s, prog_s, n_x_s]
- where
-pp_edit_map n_e (prodprog, b) rest =
-  let n_e_s = "Label from Base: " ++ n_e
-      prodprog_s = unlines $ map (\t -> "\t" ++ t) $ lines $ pp_prod_prog prodprog
-  in unlines [n_e_s, prodprog_s, rest]
-
 -- Main Product Generation
 -- For each label in the base program, generate the product program associated with it.
 gen_product :: Program -> Edit -> Edit -> Edit -> (Label, EditMap, [Label])
--- src/Printer.hs
+++ src/Printer.hs
@@ -3,6 +3,33 @@ module Printer where
 
 import qualified Data.Map as M
 import Types
+import Product
+
+pp_prod_prog :: ProdProgram -> String
+pp_prod_prog (n_e, m, n_x) =
+  let n_e_s = "Entry label: " ++ n_e
+      n_x_s = "Exit label: " ++ show n_x
+      prog_s = M.foldWithKey pp_prod_prog_line "" m
+  in n_e_s ++ "\n" ++ prog_s ++ n_x_s
+  where
+pp_prod_prog_line pre r@[(ba,pos_ba), (a,pos_a), (b,pos_b), (m,pos_m)] rest =
+  let ba_s = "\t" ++ pre ++ ": " ++ show ba ++ ": " ++ show pos_ba
+      a_s = "; " ++ show a ++ ": " ++ show pos_a
+      b_s = "; " ++ show b ++ ": " ++ show pos_b
+      m_s = "; " ++ show m ++ ": " ++ show pos_m
+  in ba_s ++ a_s ++ b_s ++ m_s ++ "\n" ++ rest
+
+pp_gen_product :: (Label, EditMap, [Label]) -> String
+pp_gen_product (n_e, p, n_x) =
+  let n_e_s = "Entry label: " ++ n_e
+      n_x_s = "Exit label: " ++ show n_x
+      prog_s = M.foldWithKey pp_edit_map "" p
+  in unlines [n_e_s, prog_s, n_x_s]
+ where
+pp_edit_map n_e (prodprog, b) rest =
+  let n_e_s = "Label from Base: " ++ n_e
+      prodprog_s = unlines $ map (\t -> "\t" ++ t) $ lines $ pp_prod_prog prodprog
+  in unlines [n_e_s, prodprog_s, rest]
 
 instance Show OpCode where
   show op = case op of
