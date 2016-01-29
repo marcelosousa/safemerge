02403f7d6bd1b316363145e879c19d51b1ee6a73
marcelosousa <dipython@gmail.com>
2016-01-29 09:45:24 -0500

    debugging

--- benchmarks/correct/B5/run.sh
+++ benchmarks/correct/B5/run.sh
@@ -13,6 +13,6 @@ dot -Tpdf merge.dot > graphs/merge.pdf;
 dot -Tpdf product.dot > graphs/product.pdf;
 rm *.dot;
 echo "generating the encodings";
-wiz finermerge -p=orig.txt -a=a.txt -b=b.txt -m=merge.txt -o=vcs/finermerge_enc.smt2;
+wiz gfinermerge -p=orig.txt -a=a.txt -b=b.txt -m=merge.txt -o=vcs/finermerge_enc.smt2;
 echo "calling solver";
 time z3 vcs/finermerge_enc.smt2 fixedpoint.engine=duality;
