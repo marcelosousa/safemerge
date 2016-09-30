863763c2de773a6f35e91ab4364841d09e5e39d7
marcelosousa <dipython@gmail.com>
2016-09-30 14:05:16 -0400

    started with the calculus rules

---
+++ src/Calculus.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+-------------------------------------------------------------------------------
+-- Module    :  Calculus Rules 
+-- Copyright :  (c) 2016 Marcelo Sousa
+-------------------------------------------------------------------------------
+module Calculus where
+
+import Language.Java.Parser hiding (opt)
+import Language.Java.Pretty hiding (opt)
+import Language.Java.Syntax
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import qualified Debug.Trace as T
+import Edit
+
+miniproduct :: [Edit] -> Edit
+miniproduct es = undefined 
+
+reduce :: [Edit] -> Edit -> Edit 
+reduce pp p =
+  let p' = miniproduct pp
+  in miniproduct [p',p]
+
+seq :: Edit -> Edit -> Edit 
+seq p1 p2 = p1 ++ p2 
+
+loop :: Edit -> BlockStmt -> BlockStmt 
+loop w1 w2 = undefined 
+
+sequence :: [Edit] -> Edit
+sequence es =
+  let (hs,ts) = unzip $ map (\e -> ([head e],tail e)) es
+  in miniproduct hs ++ miniproduct ts           
