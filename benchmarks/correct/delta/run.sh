541247570f6a1c40ea9823dc4a6b31b30a7662dc
marcelosousa <dipython@gmail.com>
2016-10-07 08:44:05 -0400

    working

---
+++ benchmarks/correct/delta/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "generating the graphs";
+wiz parse -m=program -f=prog.txt > prog.dot;
+wiz parse -m=edit -f=a.txt > a.dot;
+wiz parse -m=edit -f=b.txt > b.dot;
+wiz product -p=prog.txt -a=a.txt -b=b.txt -m=merge.txt > product.dot;
+dot -Tpdf prog.dot > graphs/prog.pdf;
+dot -Tpdf a.dot > graphs/a.pdf;
+dot -Tpdf b.dot > graphs/b.pdf;
+dot -Tpdf product.dot > graphs/product.pdf;
+rm *.dot;
+echo "generating the encodings";
+#wiz merge -p=prog.txt -a=a.txt -b=b.txt -m=merge.txt -o=vcs/merge_enc.smt2;
+wiz gfinermerge -p=prog.txt -a=a.txt -b=b.txt -m=merge.txt -o=vcs/finermerge_enc.smt2;
+echo "calling solver";
+#time z3 vcs/merge_enc.smt2 fixedpoint.engine=duality;
+time z3 vcs/finermerge_enc.smt2 fixedpoint.engine=duality;
