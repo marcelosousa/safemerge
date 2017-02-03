6872e0051bdadc4fb4f07ce331cc5312bbeb3b3c
marcelosousa <dipython@gmail.com>
2017-02-03 17:27:12 +0100

    modified several benchmarks

--- benchmarks/github/voldemort/0a04cf9587_JmxService_startInner_o_JmxService.java
+++ benchmarks/github/voldemort/0a04cf9587_JmxService_startInner_o_JmxService.java
@@ -3,6 +3,6 @@
   registerBean(cluster, JmxUtils.createObjectName(Cluster.class));
   for (VoldemortService service : services) registerBean(service, JmxUtils.createObjectName(service.getClass()));
   for (Store<ByteArray, byte[], byte[]> store : this.storeRepository.getAllStorageEngines()) {
-                                                                                               registerBean(store, JmxUtils.createObjectName(JmxUtils.getPackageName(store.getClass()), store.getName()));
-                                                                                             }
+	  registerBean(store, JmxUtils.createObjectName(JmxUtils.getPackageName(store.getClass()), store.getName()));
+  }
 }
