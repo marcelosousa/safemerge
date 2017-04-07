1a306ce147bbc3a60614da16fc7e4d73f4f4e192
marcelosousa <dipython@gmail.com>
2017-04-07 18:22:05 +0100

    working on the product construction

--- src/Analysis/Java/AST.hs
+++ src/Analysis/Java/AST.hs
@@ -43,7 +43,32 @@ data AnnBlockStmt =
  | AnnLocalClass ClassDecl
  | AnnLocalVars [Int] [Modifier] Type [VarDecl]
 
-data AnnStmt = AnnStmt [Int] Stmt
+data AnnStmt 
+  = AnnStmtBlock    [Int] AnnBlock
+  | AnnIfThen       [Int] Exp AnnStmt
+  | AnnIfThenElse   [Int] Exp AnnStmt AnnStmt
+  | AnnWhile        [Int] Exp AnnStmt
+  | AnnBasicFor     [Int] (Maybe ForInit) (Maybe Exp) (Maybe [Exp]) AnnStmt
+  | AnnEnhancedFor  [Int] [Modifier] Type Ident Exp AnnStmt
+  | AnnEmpty        [Int]
+  | AnnExpStmt      [Int] Exp
+  | AnnAssert       [Int] Exp (Maybe Exp)
+  | AnnAssume       [Int] Exp
+  | AnnSwitch       [Int] Exp [AnnSwitchBlock]
+  | AnnDo           [Int] AnnStmt Exp
+  | AnnBreak        [Int] (Maybe Ident)
+  | AnnContinue     [Int] (Maybe Ident)
+  | AnnReturn       [Int] (Maybe Exp)
+  | AnnSynchronized [Int] Exp AnnBlock
+  | AnnThrow        [Int] Exp
+  | AnnTry          [Int] AnnBlock [AnnCatch] (Maybe AnnBlock)
+  | AnnLabeled      [Int] Ident AnnStmt
+  | AnnHole         [Int]
+  | AnnSkip         [Int]
+
+data AnnSwitchBlock = AnnSwitchBlock [Int] SwitchLabel [AnnBlockStmt]
+
+data AnnCatch = AnnCatch [Int] FormalParam AnnBlock
 
 class Annotate a b where
   toAnn :: [Int] -> a -> b
@@ -64,139 +89,87 @@ instance Annotate MemberDecl AnnMemberDecl where
     AnnMemberInterfaceDecl i -> MemberInterfaceDecl i
 
 instance Annotate MethodBody AnnMethodBody where
-  toAnn = undefined
-  fromAnn = undefined
-
+  toAnn p (MethodBody mBlock) =
+    case mBlock of
+      Nothing -> AnnMethodBody Nothing
+      Just b  -> AnnMethodBody $ Just $ toAnn p b 
+  fromAnn (AnnMethodBody mBlock) = 
+    case mBlock of
+      Nothing -> MethodBody Nothing
+      Just b  -> MethodBody $ Just $ fromAnn b 
+  
 instance Annotate ConstructorBody AnnConstructorBody where
-  toAnn = undefined
-  fromAnn = undefined
+  toAnn p (ConstructorBody mInv blockStmts) = AnnConstructorBody mInv $ map (toAnn p) blockStmts 
+  fromAnn (AnnConstructorBody mInv blockStmts) = ConstructorBody mInv $ map fromAnn blockStmts 
 
-{-
-simplifyMDecl :: MemberDecl -> MemberDecl 
-simplifyMDecl m = case m of
-  MethodDecl mods tys ty mId parms exTy bdy ->
-    MethodDecl mods tys ty mId parms exTy (simplifyMBody bdy)
-  ConstructorDecl mods tys mId parms exTy bdy -> 
-    ConstructorDecl mods tys mId parms exTy (simplifyCBody bdy)
-  _ -> m
+instance Annotate Block AnnBlock where
+  toAnn p (Block stmts) = AnnBlock $ map (toAnn p) stmts 
+  fromAnn (AnnBlock stmts) = Block $ map fromAnn stmts 
 
-simplifyMBody :: MethodBody -> MethodBody
-simplifyMBody (MethodBody m) = case m of
-  Nothing -> MethodBody $ Nothing
-  Just b  -> MethodBody $ Just $ simplifyBlock b
+instance Annotate BlockStmt AnnBlockStmt where
+  toAnn p blockStmt =  
+    case blockStmt of
+      BlockStmt stmt -> AnnBlockStmt $ toAnn p stmt
+      LocalClass cl  -> AnnLocalClass cl
+      LocalVars mods ty decls -> AnnLocalVars p mods ty decls
+  fromAnn blockStmt = 
+    case blockStmt of
+      AnnBlockStmt stmt -> BlockStmt $ fromAnn stmt
+      AnnLocalClass cl  -> LocalClass cl
+      AnnLocalVars p mods ty decls -> LocalVars mods ty decls
 
-simplifyBlock :: Block -> Block
-simplifyBlock (Block stmts) = Block $ map simplifyBStmt stmts
+instance Annotate Stmt AnnStmt where
+  toAnn p stmt = 
+    case stmt of
+      StmtBlock    b         -> AnnStmtBlock    p (toAnn p b)
+      IfThen       e s       -> AnnIfThen       p e (toAnn p s)
+      IfThenElse   e s t     -> AnnIfThenElse   p e (toAnn p s) (toAnn p t)
+      While        e s       -> AnnWhile        p e (toAnn p s)       
+      BasicFor     e f g s   -> AnnBasicFor     p e f g (toAnn p s) 
+      EnhancedFor  m t i e s -> AnnEnhancedFor  p m t i e (toAnn p s) 
+      Empty                  -> AnnEmpty        p
+      ExpStmt      e         -> AnnExpStmt      p e
+      Assert       e f       -> AnnAssert       p e f
+      Assume       e         -> AnnAssume       p e
+      Switch       e b       -> AnnSwitch       p e (map (toAnn p) b) 
+      Do           s e       -> AnnDo           p (toAnn p s) e
+      Break        i         -> AnnBreak        p i
+      Continue     i         -> AnnContinue     p i
+      Return       e         -> AnnReturn       p e
+      Synchronized e b       -> AnnSynchronized p e (toAnn p b)
+      Throw        e         -> AnnThrow        p e 
+      Try          b c mb    -> AnnTry          p (toAnn p b) (map (toAnn p) c) (fmap (toAnn p) mb) 
+      Labeled      i s       -> AnnLabeled      p i (toAnn p s) 
+      Hole                   -> AnnHole         p
+      Skip                   -> AnnSkip         p
+  fromAnn stmt = 
+    case stmt of
+      AnnStmtBlock    p b         -> StmtBlock    (fromAnn b)
+      AnnIfThen       p e s       -> IfThen       e (fromAnn s)
+      AnnIfThenElse   p e s t     -> IfThenElse   e (fromAnn s) (fromAnn t)
+      AnnWhile        p e s       -> While        e (fromAnn s)       
+      AnnBasicFor     p e f g s   -> BasicFor     e f g (fromAnn s) 
+      AnnEnhancedFor  p m t i e s -> EnhancedFor  m t i e (fromAnn s) 
+      AnnEmpty        p           -> Empty        
+      AnnExpStmt      p e         -> ExpStmt      e
+      AnnAssert       p e f       -> Assert       e f
+      AnnAssume       p e         -> Assume       e
+      AnnSwitch       p e b       -> Switch       e (map (fromAnn) b) 
+      AnnDo           p s e       -> Do           (fromAnn s) e
+      AnnBreak        p i         -> Break        i
+      AnnContinue     p i         -> Continue     i
+      AnnReturn       p e         -> Return       e
+      AnnSynchronized p e b       -> Synchronized e (fromAnn b)
+      AnnThrow        p e         -> Throw        e 
+      AnnTry          p b c mb    -> Try          (fromAnn b) (map (fromAnn) c) (fmap (fromAnn) mb) 
+      AnnLabeled      p i s       -> Labeled      i (fromAnn s) 
+      AnnHole         p           -> Hole         
+      AnnSkip         p           -> Skip         
 
-simplifyCBody :: ConstructorBody -> ConstructorBody
-simplifyCBody (ConstructorBody m stmts) =
-  ConstructorBody m $ map simplifyBStmt stmts
+instance Annotate SwitchBlock AnnSwitchBlock where
+  toAnn p (SwitchBlock l stmts) = AnnSwitchBlock p l (map (toAnn p) stmts)
+  fromAnn (AnnSwitchBlock p l stmts) = SwitchBlock l (map fromAnn stmts)
 
-simplifyEdit :: Edit -> Edit
-simplifyEdit = map simplifyBStmt
-
-simplifyBStmt :: BlockStmt -> BlockStmt
-simplifyBStmt bstmt = case bstmt of
-  BlockStmt s -> BlockStmt $ simplifyStmt s
-  _ -> bstmt
-
-simplifyStmt :: Stmt -> Stmt 
-simplifyStmt stmt = case stmt of
-  StmtBlock b -> StmtBlock $ simplifyBlock b
-  IfThen c t  -> IfThenElse (simplifyExp c) (simplifyStmt t) Empty
-  IfThenElse c t e ->
-    IfThenElse (simplifyExp c) (simplifyStmt t) (simplifyStmt e)
-  While cond stmt -> While (simplifyExp cond) (simplifyStmt stmt)
-  BasicFor mInit cond end bdy ->
-    -- i :: Stmt
-    let i = case mInit of
-          Nothing -> Empty
-          Just init -> case init of
-            ForLocalVars mods tys vars -> StmtBlock $ Block [LocalVars mods tys vars]
-            ForInitExps exps -> 
-              StmtBlock (Block $ map (BlockStmt . ExpStmt . simplifyExp) exps)
-    -- c :: Exp
-        c = case cond of
-          Nothing -> Lit $ Boolean True
-          Just e  -> simplifyExp e
-    -- bdy :: Stmt
-        b = let bdy' = simplifyStmt bdy
-            in case end of
-                Nothing -> bdy' 
-                Just exps ->
-                  let _bdy = BlockStmt bdy'
-                  in StmtBlock (Block $ _bdy:(map (BlockStmt . ExpStmt . simplifyExp) exps))
-    in StmtBlock $ Block [BlockStmt i, BlockStmt $ While c b] 
-  EnhancedFor mods ty i e bdy ->
-    let idI = Ident "wiz_i" 
-        varI = VarDecl (VarId idI) $ Just $ InitExp $ Lit $ Int 0 
-        nameI = ExpName $ Name [idI]
-        varDeclI = LocalVars [] (PrimType IntT) [varI]
-        -- varDecl for i
-        varK = VarDecl (VarId i) $ Just $ InitExp $ ArrayAccess $ ArrayIndex exp [nameI] 
-        varDeclK = LocalVars mods ty [varK]
-        -- condition for the while
-        exp = simplifyExp e
-        cond = BinOp nameI LThan $ MethodInv $ PrimaryMethodCall exp [] (Ident "length") [] 
-        -- create the increment
-        inc = ExpStmt $ BinOp nameI Add $ Lit $ Int 1 
-        body = simplifyStmt bdy
-        body' = StmtBlock $ Block [BlockStmt body, BlockStmt inc] 
-    in StmtBlock $ Block [varDeclI, varDeclK, BlockStmt $ While cond body'] 
-  Empty -> Empty
-  ExpStmt exp -> ExpStmt $ simplifyExp exp
-  Do bdy c ->
-    let body = simplifyStmt bdy
-        cond = simplifyExp c
-    in StmtBlock $ Block $ map BlockStmt [body, While cond body]  
-  _ -> stmt
-
-simplifyExp :: Exp -> Exp
-simplifyExp exp = 
-  let one = Lit $ Int 1
-  in case exp of
-    PostIncrement e ->
-      let lhs = expToLhs e
-      in  Assign lhs EqualA $ BinOp e Add one
-    PreIncrement  e -> 
-      let lhs = expToLhs e
-      in  Assign lhs EqualA $ BinOp e Add one
-    PostDecrement e ->
-      let lhs = expToLhs e
-      in  Assign lhs EqualA $ BinOp e Sub one
-    PreDecrement  e -> 
-      let lhs = expToLhs e
-      in  Assign lhs EqualA $ BinOp e Sub one
-    Assign lhs op rhs -> 
-      case op of
-        EqualA -> exp
-        _ ->
-          let e = lhsToExp lhs 
-              op' = toOp op
-          in Assign lhs EqualA $ BinOp e op' rhs
-    _ -> exp
-
-toOp :: AssignOp -> Op
-toOp op = case op of
-  MultA -> Mult
-  DivA  -> Div
-  RemA  -> Rem
-  AddA  -> Add
-  SubA  -> Sub
-  LShiftA -> LShift
-  RShiftA -> RShift
-  AndA -> And
-  XorA -> Xor
-  OrA  -> Or
-
-lhsToExp :: Lhs -> Exp
-lhsToExp lhs = case lhs of
-  NameLhs n -> ExpName n
-  FieldLhs f -> FieldAccess f
-  ArrayLhs ai -> ArrayAccess ai
-
-expToLhs :: Exp -> Lhs
-expToLhs e = case e of
-  ExpName n -> NameLhs n
-  _ -> error $ "expToLhs: Unsupported " ++ show e
--}
+instance Annotate Catch AnnCatch where
+  toAnn p (Catch fp b) = AnnCatch p fp (toAnn p b) 
+  fromAnn (AnnCatch p fp b) = Catch fp (fromAnn b)
