b9af11f5e5252e66e20bdc3dbdfe00a39d05b6f2
marcelosousa <dipython@gmail.com>
2017-06-07 13:03:37 -0400

    working on the motivation example

---
+++ tests/regression/motivation/TSched_o.java
@@ -0,0 +1,21 @@
+public class TestScheduler extends Scheduler {
+    private final Queue<TimedAction<?>> queue = new PriorityQueue<TimedAction<?>>(11, new CompareActionsByTime());
+
+    // Storing time in nanoseconds internally.
+    private long time;
+
+    private void triggerActions(long targetTimeInNanos) {
+      int brk = 0;
+      while (!queue.isEmpty() && (brk == 0)) {
+        TimedAction<?> current = queue.peek();
+        if (getTime(current) > targetTimeInNanos) {
+          time = targetTimeInNanos;
+          brk = 1;
+        } else {
+          time = getTime(current);
+          queue.remove();
+          call(current);
+        }
+      }
+    }
+}
