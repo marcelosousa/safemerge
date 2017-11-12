291f6625b242e69f490c1992093b68d1b2d01345
marcelosousa <dipython@gmail.com>
2017-11-12 09:02:07 -0500

    working on experiments

---
+++ benchmarks/pldi_experiments/new_filters/unsafe/spring-boot/inst291/cacheManager.java
@@ -0,0 +1,21 @@
+@Autowired
+ private CacheProperties cacheProperties;
+@Autowired
+ CacheManagerCustomizerInvoker customizerInvoker;
+@Autowired
+ private CacheManagerCustomizers customizerInvoker;
+@Bean
+ public RedisCacheManager cacheManager (RedisTemplate<Object, Object> redisTemplate)
+{
+  RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+  cacheManager.setUsePrefix(true);
+  List<String> cacheNames = this.cacheProperties.getCacheNames();
+  if (!cacheNames.isEmpty())
+  {
+    cacheManager.setCacheNames(cacheNames);
+  }
+  else
+    ;
+  return this.customizerInvoker.customize(cacheManager);
+  return;
+}
